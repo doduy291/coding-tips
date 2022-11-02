47a518c1824b3d4b7679a2a26aac8606e311425e
Dayui <doduy291@gmail.com>
2022-11-02 21:23:08 +0700

    feat: build Router

--- react/general/src/App.tsx
+++ react/general/src/App.tsx
@@ -1,9 +1,30 @@
-import CancelFetching from "./components/useEffect/cancel-fetching";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import CancelFetching from "./features/cancel-fetching";
+
+import PreventDuplicateObjectValue from "./features/prevent-duplicate-object-value";
 
 function App() {
   return (
     <div className="App">
-      <CancelFetching />
+      <BrowserRouter>
+        <div className="card-container">
+          <Link to="/cancel-fetching" className="card">
+            Cancel Fetching
+          </Link>
+          <Link to="/prevent-duplicate-object-value" className="card">
+            Prevent Duplicate Object Value
+          </Link>
+        </div>
+
+        <Routes>
+          <Route path="/cancel-fetching/*" element={<CancelFetching />} />
+
+          <Route
+            path="/prevent-duplicate-object-value"
+            element={<PreventDuplicateObjectValue />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
